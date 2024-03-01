15be7b0a2e703b9365d70ce123bdb8eab3da187a
VarunTyagarayanG <156878356+varunTyagarayanG@users.noreply.github.com>
2024-03-01 15:49:35 +0530

    Added backEnd repo to server

---
+++ models/Asset.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const equitySchema = new mongoose.Schema({
+  stockName: { type: String, required: true },
+  dateOfPurchase: { type: Date, required: true },
+  sharePriceAtPurchase: { type: Number, required: true },
+  totalInvestment: { type: Number, required: true },
+});
+
+const goldSchema = new mongoose.Schema({
+  dateOfPurchase: { type: Date, required: true },
+  pricePer10g: { type: Number, required: true },
+  gramsBought: { type: Number, required: true },
+});
+
+const fixedDepositSchema = new mongoose.Schema({
+  dateOfPurchase: { type: Date, required: true },
+  principalAmount: { type: Number, required: true },
+  interestRate: { type: Number, required: true },
+  tenure: { type: Number, required: true },
+});
+
+const realEstateSchema = new mongoose.Schema({
+  purchasePrice: { type: Number, required: true },
+  todayPrice: { type: Number, required: true },
+});
+
+const assetSchema = new mongoose.Schema({
+  assetClass: { type: String, required: true },
+  equity: equitySchema,
+  gold: goldSchema,
+  fixedDeposit: fixedDepositSchema,
+  realEstate: realEstateSchema,
+});
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  assets: [assetSchema],
+});
+
+const Asset = mongoose.model("Asset", assetSchema);
+
+module.exports = Asset;
