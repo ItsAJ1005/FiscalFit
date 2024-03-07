17460cf8666df7116245d2385319d912a8320462
CS-parth <parth.vijy@gmail.com>
2024-03-08 00:10:35 +0530

    updated app.js with ejs integration

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const fs = require('fs');
-
+const fs = require("fs");
+const ejs = require("ejs");
 /**changes*/
-const mongoose = require('mongoose');
-const assetRoutes = require('./routes/assetRoutes');
-const authRoutes = require('./routes/authRoutes');
-app.use(express.json());
-
-mongoose.connect('mongodb://0.0.0.0:27017/PersonalPortfolioSystem', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('MongoDB connected successfully');
-})
-.catch(err => {
-  console.error('MongoDB connection failed:', err);
-  process.exit(1);
-});
-
-app.use('/api/assets', assetRoutes);
-app.use('/api/auth', authRoutes);
-/**changes*/
-
+const mongoose = require("mongoose");
+const assetRoutes = require("./routes/assetRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const port = 5000;
-const viewspath = path.join(__dirname,'views');
-app.use(express.static(__dirname + '/public'));
+const viewspath = path.join(__dirname, "views");
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/',(req,res) =>{
-    res.sendFile(`${viewspath}/index.html`);
-})
+mongoose
+  .connect("mongodb://0.0.0.0:27017/PersonalPortfolioSystem", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1);
+  });
 
-app.listen(port,()=>{
-    console.log(`The server is up and running on ${port}`);
-})
+app.use("/api/assets", assetRoutes);
+app.use("/api/auth", authRoutes);
+/**changes*/
+
+app.get("/", (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`The server is up and running on ${port}`);
+});
