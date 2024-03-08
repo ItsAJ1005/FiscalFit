dd5e857491ec1002471f5b37c9452d922a211575
CS-parth <parth.vijy@gmail.com>
2024-03-08 10:18:07 +0530

    Updated User Schema with AssetSchema

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const { Asset,assetSchema } = require('./Asset.js');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  assets: [assetSchema],
 });
 userSchema.methods.updatePassword = async function (newPassword) {
   this.password = await bcrypt.hash(newPassword, 10);
