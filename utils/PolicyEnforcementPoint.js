117359f47785e736efd227cdfd8ecc9939731caa
CS-parth <parth.vijy@gmail.com>
2024-04-07 15:37:00 +0530

    implemented abacMiddleware

--- utils/PolicyEnforcementPoint.js
+++ utils/PolicyEnforcementPoint.js
@@ -1,8 +1,14 @@
 const validation = require('./Validation');
-const permissions = require('./Permission');
+const permissions = require('./rbacPolicyDecisionPoint');
+const assets = require('./abacPolicyDecisionPoint');
 const Validation = new validation();
-const Permissions = new permissions();
-
+const rbacPDP = new permissions();
+const abacPDP = new assets();
+const User = require('../models/User');
+const Comment = require('../models/Comment');
+const Community = require('../models/Community');
+const Post = require('../models/Post');
+const Asset = require('../models/Asset');
  class Middleware {
     execute(req, res, next) {
       throw new Error('Method not implemented');
@@ -34,15 +40,40 @@ const Permissions = new permissions();
   
 
   class ABACMiddleware extends Middleware {
-    execute(req, res, next) {
-      console.log('ABAC middleware logic');
-      next();
+    execute(permission,resourse) {
+      return async (req,res,next) =>{
+        if(resourse == 'user'){
+          req.asset = await User.findById(req.body.resourse);
+        }else if(resourse == 'post'){
+          req.asset = await Post.findById(req.body.resourse);
+        }else if(resourse == 'comment'){
+          req.asset = await Comment.findById(req.body.resourse);
+        }else if(resourse == 'community'){
+          req.asset = await Community.findById(req.body.resourse);
+        }else if(response == 'asset'){
+          req.asset = await Asset.findById(req.body.resourse);
+        }else{
+          console.error("Bad request {check permission}");
+        }
+        const User = await Validation.getUser(req.cookies.jwt);
+        req.user = User;
+        if (!req.cookies || !req.cookies.jwt) {
+            return res.status(403).json({ error: 'JWT token not found' });
+        }
+        const userRole = req.user ? req.user.role : 'anonymous';
+        if (userRole !== 'anonymous') {
+          req.permission = permission;
+          req.middleware = "abac";
+          return next();
+      } else {
+          return res.status(403).json({ error: 'Access denied' });
+      }
+      }
     }
   }
 
   class ChineseWallPolicy extends Middleware {
     execute(req, res, next) {
-      console.log('Chinese Wall Policy middleware logic');
       next();
     }
   }
@@ -51,18 +82,29 @@ const Permissions = new permissions();
   class PDP {
     static execute(req, res, next) {
       if(req.middleware === 'rbac'){
-        const userPermissions = Permissions.getPermissionsByRoleName(req.user.role);
+        const userPermissions = rbacPDP.getPermissionsByRoleName(req.user.role);
         if (userPermissions.includes(req.permission)) {
             return next();
         } else {
             return res.status(403).json({ error: 'Access denied' });
         }
       }else if(req.middleware === 'abac'){
-
+        // creat conditional set
+        const conditionalSet = {
+          ...(req.user.role && {role : req.user.role}),
+          ...(req.permission && {action : req.permission}),
+          ...(req.asset?.isBanned===true && {isBanned : isBanned})
+        }
+        if(abacPDP.isAllowed(conditionalSet)){
+          return next();
+        }else{
+          return res.status(403).json({ error: 'Access denied' });
+        }
       }else if(req.middleware === 'chineseWall'){
 
+      }else{
+        res.status(400).json({message: "Using anonyomous policy"});
       }
-      next();
     }
   }
   
