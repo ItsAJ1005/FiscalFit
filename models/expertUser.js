8f4afedf31f94504de01dfdd70d63fac7a409de1
CS-parth <parth.vijy@gmail.com>
2024-04-22 17:54:20 +0530

    updated expertSchema

--- models/expertUser.js
+++ models/expertUser.js
@@ -4,6 +4,10 @@ const naiveUser = require('./naiveUser');
 const User = require("./User");
 
 const expertUserSchema = mongoose.Schema({
+    communities_owned : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Community'
+    }],
     clients : [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'naiveUser'
