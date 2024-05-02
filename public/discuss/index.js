a479fe525c1f1355abca1c1b60e60d3fc3b3c073
CS-parth <parth.vijy@gmail.com>
2024-05-02 12:00:41 +0530

    Added social section

--- public/discuss/index.js
+++ public/discuss/index.js
@@ -1,8 +1,50 @@
 const loginButton = document.getElementById('log-in');
 const logoutButton = document.getElementById('log-out');
 var loginModal = document.getElementById('loginModal');
+const signupModal = document.getElementById('signupModal');
 loginModal.style.display = "none";
+signupModal.style.display = "none";
 const logginFormbtn = document.getElementById('login-form-btn');
+const signupButton = document.getElementById('sign-up');
+const signupFormbtn = document.getElementById('signup-form-btn');
+
+if(signupButton) {
+    signupFormbtn.addEventListener('click', (e) => {
+        console.log("first");
+        e.preventDefault();
+        const formBody = {
+            "username": document.getElementById('username').value,
+            "email": document.getElementById('email').value,
+            "password": document.getElementById('password').value,
+            "role": document.getElementById('role').value
+        };
+        fetch('../api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formBody)
+            })
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = '/discuss/home';
+                } else {
+                    // Check if response is JSON
+                    return response.json().then(data => {
+                        if (data && data.message) {
+                            throw new Error(data.message);
+                        } else {
+                            throw new Error('Unknown error occurred');
+                        }
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Login request error:', error);
+                alert('Login request error: ' + error.message);
+            });
+    });
+}
 
 if (logginFormbtn) {
     logginFormbtn.addEventListener('click', (e) => {
@@ -51,7 +93,13 @@ if(logoutButton){
             if (response.ok) {
                 window.location.href = '/discuss/home'; 
             } else {
-                console.error('Logout request failed:', response.statusText);
+                return response.json().then(data => {
+                    if (data && data.message) {
+                        throw new Error(data.message);
+                    } else {
+                        throw new Error('Unknown error occurred');
+                    }
+                });
             }
         })
         .catch(error => {
@@ -67,6 +115,13 @@ if(loginButton){
     })
 }
 
+if(signupButton){
+    signupButton.addEventListener('click',()=>{
+        if(signupModal.style.display == "none")
+            signupModal.style.display = "block";
+    })    
+}
+
 window.addEventListener('click', function(e){
     if(loginButton === null || loginButton.contains(e.target)){
         return;
@@ -79,7 +134,18 @@ window.addEventListener('click', function(e){
         }   
     }
 });
-
+window.addEventListener('click', function(e){
+    if(signupButton === null || signupButton.contains(e.target)){
+        return;
+    }
+    var signupModal = document.getElementById('signupModal');
+    if (signupModal.contains(e.target)){
+    } else {
+        if(signupModal.style.display == "block") {
+            signupModal.style.display = "none";
+        }   
+    }
+});
 const creatPostButton = document.querySelector('.create-post-btn-container');
 
 
